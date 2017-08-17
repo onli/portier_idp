26baf0877c0d5af2b478d0933fbacdb2c3d7efc7
onli <onli@paskuda.biz>
2017-08-17 17:00:25 +0200

    init webfinger endpoint

--- server.rb
+++ server.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 require 'base64'
+require 'inifile'
 
 rsa_public = nil
 # TODO: It would be better not to have the key as global variables, but to load them only when needed
 rsa_private = nil
+ownUrl = nil
 
 configure do
     # Create the jwks, if not existing already
@@ -15,12 +17,27 @@ configure do
     rsa_public = rsa_private.public_key
 
     # Read the absolute URL from the config.ini
+    config = IniFile.load('config.ini')
+    ownUrl = config['main']['url']
 end
 
 # the webfinger endpoint confirms to portier that a user is registered,
 # and that this service supports portier as auth method
 get '/.well-known/webfinger' do
+    if (params[:rel] == 'https://portier.io/specs/auth/1.0/idp')
+        email = params[:resource].gsub('acct:', '')
 
+        return JSON.generate({
+            "subject": "acct:" + email,
+            "links":
+                [
+                {
+                "rel": "https://portier.io/specs/auth/1.0/idp",
+                "href": ownUrl
+                }
+                ]
+            }).to_s
+    end
 end
 
 # Portier then gets this endpoint, which has two relevant information:
@@ -45,7 +62,7 @@ end
 # Echo the jwks, the keyset used to sign the web tokens. Portier fetches this
 # at the end to confirm the jwt supposed to confirm the login
 get '/.well-known/jwks' do
-    
+    header 'Content-Type: application/json'
     JSON.generate({
         "kty": "RSA",
         "alg": "RS256",
